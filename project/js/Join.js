78c0725cd4bdd30995c6bb3ee64a8fa4d296166d
UIYOUNG <kanda8@nate.com>
2024-08-26 23:15:44 +0900

    1.Join.js -이미지 변경 시 변경이 아닌 추가되는거 수정 -주소 관련 다음 api 추가(업데이트 중) 2.Join.html -주소칸 추가

--- project/js/Join.js
+++ project/js/Join.js
@@ -2,10 +2,9 @@
 /* 240826  회원가입 페이지 JS */
 
 
-
 //메일 주소 변경
 function selectEmail(mail){
-    alert(mail);
+    
     $("#emailBack").val(mail);
     var emailFront=$("#emailFront").val();
     var emailBack=$("#emailBack").val();
@@ -40,8 +39,54 @@ function previewImg(event){
     reader.onload = function(event) {
         var img = document.createElement("img");
         img.setAttribute("src", event.target.result);
+        //0826 이미지 변경 시 추가되던거 수정
+        document.querySelector("div#previewImg").innerHTML='';  
         document.querySelector("div#previewImg").appendChild(img);
     };
 
     reader.readAsDataURL(event.target.files[0]);
+}
+
+
+function addressFind(){
+    new daum.Postcode({
+        oncomplete: function(data) {
+            console.log('aaaa');
+            // // 팝업을 통한 검색 결과 항목 클릭 시 실행
+            // alert('aaaa');
+            var addr = ''; // 주소_결과값이 없을 경우 공백 
+            var extraAddr = ''; // 참고항목
+            // alert('aaaa');
+            // // //사용자가 선택한 주소 타입에 따라 해당 주소 값을 가져온다.
+            // if (data.userSelectedType === 'R') { // 도로명 주소를 선택
+            //     addr = data.roadAddress;
+            // } else { // 지번 주소를 선택
+            //     addr = data.jibunAddress;
+            // }
+            // alert('bbbb');
+            // if(data.userSelectedType === 'R'){
+            //     if(data.bname !== '' && /[동|로|가]$/g.test(data.bname)){
+            //         extraAddr += data.bname;
+            //     }
+            //     if(data.buildingName !== '' && data.apartment === 'Y'){
+            //         extraAddr += (extraAddr !== '' ? ', ' + data.buildingName : data.buildingName);
+            //     }
+            //     if(extraAddr !== ''){
+            //         extraAddr = ' (' + extraAddr + ')';
+            //     }
+            // } else {
+            //     document.getElementById("UserOrigin").value = '';
+            // }
+            
+            // 선택된 우편번호와 주소 정보를 input 박스에 넣는다.
+            document.getElementById('addressNum').value = data.zonecode;
+            document.getElementById("addressOrigin").value = addr;
+            
+            document.getElementById("addressOrigin").value += extraAddr;
+            document.getElementById("addressAdd").focus(); // 우편번호 + 주소 입력이 완료되었음으로 상세주소로 포커스 이동
+
+        }
+    }).open();
+
+
 }
